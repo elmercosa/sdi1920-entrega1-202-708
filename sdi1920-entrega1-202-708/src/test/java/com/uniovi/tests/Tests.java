57f24def217a4364a162a6ad1fb7e53f6f231c9f
Jaime <jaimeknd77@gmail.com>
2020-03-07 21:57:37 +0100

    Primera prueba

---
+++ sdi1920-entrega1-202-708/src/test/java/com/uniovi/tests/Tests.java
@@ -0,0 +1,119 @@
+package com.uniovi.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_LoginView;
+import com.uniovi.tests.pageobjects.PO_PrivateView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+import com.uniovi.tests.pageobjects.PO_RegisterView;
+import com.uniovi.tests.pageobjects.PO_View;
+import com.uniovi.tests.utils.SeleniumUtils;
+
+//Ordenamos las pruebas por el nombre del método
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+public class Tests {
+
+	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
+	// automáticas)):
+	static String PathFirefox65 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver024 = "D:\\Jaime\\Repositorios\\sdi1920-entrega1-202-708\\sdi1920-entrega1-202-708\\geckodriver024win64.exe";
+
+	// Común a Windows y a MACOSX
+	static WebDriver driver = getDriver(PathFirefox65, Geckdriver024);
+	static String URL = "http://localhost:8090";
+
+	public static WebDriver getDriver(String PathFirefox, String Geckdriver) {
+		System.setProperty("webdriver.firefox.bin", PathFirefox);
+		System.setProperty("webdriver.gecko.driver", Geckdriver);
+		WebDriver driver = new FirefoxDriver();
+		return driver;
+	}
+
+	// Antes de cada prueba se navega al URL home de la aplicaciónn
+	@Before
+	public void setUp() {
+		driver.navigate().to(URL);
+	}
+
+	// Después de cada prueba se borran las cookies del navegador
+	@After
+	public void tearDown() {
+		driver.manage().deleteAllCookies();
+	}
+
+	// Antes de la primera prueba
+	@BeforeClass
+	static public void begin() {
+	}
+
+	// Al finalizar la última prueba
+	@AfterClass
+	static public void end() {
+		// Cerramos el navegador al finalizar las pruebas
+		driver.quit();
+	}
+
+	// Ejercicio 1
+
+	// [Prueba1] Registro de Usuario con datos válidos.
+	@Test
+	public void test01_01() throws Exception {
+		driver.get("http://localhost:8090/");
+ 
+		TestsUtils.clickOption(driver, "signup", "class", "btn btn-primary");
+		
+		TestsUtils.fillFormRegister(driver, "prueba1@prueba.com", "Prueba1", "Prueba1", "123456", "123456");
+
+		SeleniumUtils.EsperaCargaPagina(driver, "text", "Esto es una zona privada la web", 2);
+
+	}
+
+	// [Prueba2] Registro de Usuario con datos inválidos (email vacío, nombre vacío,
+	// apellidos vacíos).
+	@Test
+	public void test01_02() throws Exception {
+//		driver.get("http://localhost:8090/");
+
+//		TestsUtils.fillFormRegister(driver, "", "", "", "123456", "123456");
+
+//		SeleniumUtils.EsperaCargaPagina(driver, "text", "Esto es una zona privada la web", 2);
+
+	}
+
+	// [Prueba3] Registro de Usuario con datos inválidos (repetición de contraseña
+	// inválida).
+	@Test
+	public void test01_03() throws Exception {
+//		driver.get("http://localhost:8090/");
+
+//		TestsUtils.fillFormRegister(driver, "prueba2@prueba.com", "Prueba2", "Prueba2", "123456", "123457");
+
+//		SeleniumUtils.EsperaCargaPagina(driver, "text", "Esto es una zona privada la web", 2);
+	}
+
+	// [Prueba4] Registro de Usuario con datos inválidos (email existente).
+	@Test
+	public void test01_04() throws Exception {
+//		driver.get("http://localhost:8090/");
+
+//		TestsUtils.fillFormRegister(driver, "prueba1@prueba.com", "Prueba1", "Prueba1", "123456", "123456");
+
+//		SeleniumUtils.EsperaCargaPagina(driver, "text", "Esto es una zona privada la web", 2);
+	}
+}
---
+++ sdi1920-entrega1-202-708/src/test/java/com/uniovi/tests/TestsUtils.java
@@ -0,0 +1,60 @@
+package com.uniovi.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.uniovi.tests.utils.SeleniumUtils;
+
+public class TestsUtils {
+	
+	public static void clickOption(WebDriver driver, String textOption, String criterio, String textoDestino) {
+		// CLickamos en la opción de registro y esperamos a que se cargue el enlace de
+		// Registro.
+		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "@href", textOption, 2);
+		// Tiene que haber un sólo elemento.
+		assertTrue(elementos.size() == 1);
+		// Ahora lo clickamos
+		elementos.get(0).click();
+		// Esperamos a que sea visible un elemento concreto
+		elementos = SeleniumUtils.EsperaCargaPagina(driver, criterio, textoDestino, 2);
+		// Tiene que haber un sólo elemento.
+		assertTrue(elementos.size() == 1);
+	}
+	
+	static public List<WebElement> checkElement(WebDriver driver, String type, String text) {
+		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, type, text, 2);
+		return elementos;		
+	}
+	
+	static public void fillFormRegister(WebDriver driver, String emailp, String namep, String lastnamep, String passwordp,
+			String passwordconfp) {
+		WebElement email = driver.findElement(By.name("email"));
+		email.click();
+		email.clear();
+		email.sendKeys(emailp);
+		WebElement name = driver.findElement(By.name("name"));
+		name.click();
+		name.clear();
+		name.sendKeys(namep);
+		WebElement lastname = driver.findElement(By.name("lastName"));
+		lastname.click();
+		lastname.clear();
+		lastname.sendKeys(lastnamep);
+		WebElement password = driver.findElement(By.name("password"));
+		password.click();
+		password.clear();
+		password.sendKeys(passwordp);
+		WebElement passwordConfirm = driver.findElement(By.name("passwordConfirm"));
+		passwordConfirm.click();
+		passwordConfirm.clear();
+		passwordConfirm.sendKeys(passwordconfp);
+		//Pulsar el boton de Alta.
+		By boton = By.className("btn");
+		driver.findElement(boton).click();
+	}
+}
