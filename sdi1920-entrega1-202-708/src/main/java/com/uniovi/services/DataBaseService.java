22e4c819de5ce50090f48b8e14664c8692a67d01
jaimeknd77 <jaimeknd77@gmial.com>
2020-03-15 17:45:03 +0100

    Punto 12: Casos de prueba
    Documentación de las pruebas. Reset BBDD al inicio.

--- sdi1920-entrega1-202-708/src/main/java/com/uniovi/services/DataBaseService.java
+++ sdi1920-entrega1-202-708/src/main/java/com/uniovi/services/DataBaseService.java
@@ -31,6 +31,26 @@ public class DataBaseService {
 		admin.setRole("ROLE_ADMIN");
 		admin.setPassword(bCryptPasswordEncoder.encode(admin.getPassword()));
 		usersRepository.save(admin);
+		
+		User user1 = new User("User1", "User1", "user1@email.com", "user1");
+		user1.setRole("ROLE_USER");
+		user1.setPassword(bCryptPasswordEncoder.encode(user1.getPassword()));
+		usersRepository.save(user1);
+		
+		User user2 = new User("User2", "User2", "user2@email.com", "user2");
+		user2.setRole("ROLE_USER");
+		user2.setPassword(bCryptPasswordEncoder.encode(user2.getPassword()));
+		usersRepository.save(user2);
+		
+		User user3 = new User("User3", "User3", "user3@email.com", "user3");
+		user3.setRole("ROLE_USER");
+		user3.setPassword(bCryptPasswordEncoder.encode(user3.getPassword()));
+		usersRepository.save(user3);
+		
+		User user4 = new User("User4", "User4", "user4@email.com", "user4");
+		user4.setRole("ROLE_USER");
+		user4.setPassword(bCryptPasswordEncoder.encode(user4.getPassword()));
+		usersRepository.save(user4);
 	}
 
 }
--- sdi1920-entrega1-202-708/src/test/java/com/uniovi/tests/TestsUtils.java
+++ sdi1920-entrega1-202-708/src/test/java/com/uniovi/tests/TestsUtils.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import com.uniovi.tests.utils.SeleniumUtils;
-
 public class TestsUtils {
 
 	protected static Internationalization p = new Internationalization("messages");
